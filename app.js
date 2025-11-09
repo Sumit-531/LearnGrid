8c24d85178af0497c4dda9ad866335baa0ad411d
Sumit-531 <sumitchakraborty218@gmail.com>
2025-11-10 02:33:14 +0600

    Global error handler

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ import { PORT } from "./config/env.js";
 import studentRoute from "./routes/student.routes.js";
 import teacherRoute from "./routes/teacher.routes.js";
 import connectDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
+//custom middleware
+app.use(errorMiddleware);
 
 //routes
 app.use("/api/students", studentRoute);
